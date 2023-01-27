22c91a9226a3b9d0de625b57cfb24f07e26d1669
Allan Viray <allang.v.dev@gmail.com>
2023-01-27 15:14:53 -0600

    Create initial HomePage

---
+++ src/components/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div className="container">
+      <div className="content">
+        <h2>Home Page</h2>
+        <p>This will show top games</p>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
