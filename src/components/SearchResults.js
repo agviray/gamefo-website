fd2ae6c56b1b2c786e571380cbd6484b3874bd4f
Allan Viray <allang.v.dev@gmail.com>
2023-02-17 19:10:37 -0600

    Scroll to top of page upon new search/navigating through search results

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect, useContext } from 'react';
 import { StyledContainer, StyledResults } from './styles/SearchResults.styled';
 import { ResponseContext } from './Layout';
+import useScrollYPosition from './hooks/useScrollYPosition';
 import PageNumbers from './PageNumbers';
 import Card from './Card';
 
 const SearchResults = () => {
   const [results, setResults] = useState([]);
   const responseContextValue = useContext(ResponseContext);
+  const scrollYPosition = useScrollYPosition();
 
   useEffect(() => {
     if (Object.keys(responseContextValue.response.dataReceived).length === 0) {
@@ -14,6 +16,16 @@ const SearchResults = () => {
     }
     if (responseContextValue.response.dataReceived.results[0] !== results[0]) {
       setResults([...responseContextValue.response.dataReceived.results]);
+    }
+  }, [responseContextValue.response]);
+
+  useEffect(() => {
+    if (scrollYPosition > 0) {
+      document.documentElement.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'instant',
+      });
     }
   }, [responseContextValue.response]);
 
